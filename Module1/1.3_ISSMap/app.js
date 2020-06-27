84a6c820c3ab0cc19cacfeb43b1618cd7b491c8f
mattholamieux <matthew.nelson@thelastmile.org>
2020-06-27 10:01:00 -0700

    updated 1.3 with mapping

---
+++ Module1/1.3_ISSMap/app.js
@@ -0,0 +1,34 @@
+//Create map and add tiles
+const myMap = L.map('issMap').setView([0, 0], 1);
+const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> constributors';
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tiles = L.tileLayer(tileUrl, { attribution });
+tiles.addTo(myMap);
+
+// Create marker with custom icon
+const issIcon = L.icon({
+    iconUrl: 'iss.png',
+    iconSize: [50, 32],
+    iconAnchor: [25, 16]
+});
+const marker = L.marker([0, 0], { icon: issIcon }).addTo(myMap);
+
+let firstTime = true;
+
+async function getISS() {
+    const iss_url = 'https://api.wheretheiss.at/v1/satellites/25544';
+    const response = await fetch(iss_url);
+    const data = await response.json();
+    const { latitude, longitude } = data;
+    marker.setLatLng([latitude, longitude]);
+    if (firstTime) {
+        myMap.setView([latitude, longitude], 4);
+        firstTime = false;
+    }
+    document.getElementById('lat').textContent = latitude.toFixed(2);
+    document.getElementById('lon').textContent = longitude.toFixed(2);
+}
+
+//get the ISS current latitude and longitude
+getISS();
+setInterval(getISS, 1000); 
